966f4ac61c9160e9246c25304d3ded0943daeb1a
BobCortesP <bob.cortes@hotmail.com>
2026-05-06 15:35:00 -0400

    Se cambian los archivos para aceptar tailwindcss.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-gray-800">Hello, LookFin ImpactLab!</h1>
+    </div>
+  );
+}
+
+export default App;
